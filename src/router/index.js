f161965728aeee1d3a26c18c8fedf9722608bd7e
mejiamanuel57 <mejiamanuel57@gmail.com>
2018-12-23 00:08:35 -0400

    adding __dirname

--- src/router/index.js
+++ src/router/index.js
@@ -2,26 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 
-const generateRoutes = () => {
-  switch (location.hostname) {
-    case 'localhost': {
-      return {
-        path: '/',
-        component: Home
-      }
-    }
-    case 'mejiamanuel57.github.io': {
-      return {
-        path: '/britecore-payments-datatable',
-        component: Home
-      }
-    }
-  }
-}
-
 Vue.use(Router)
 
 export default new Router({
-  routes: generateRoutes,
-  mode: 'history'
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    }
+  ],
+  mode: 'history',
+  base: __dirname
 })
