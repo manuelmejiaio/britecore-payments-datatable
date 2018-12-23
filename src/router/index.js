5df74057b639f3aa6b8287f13818334b7dca1e56
mejiamanuel57 <mejiamanuel57@gmail.com>
2018-12-22 23:00:17 -0400

    dynamic path

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 
+const generatePath = () => {
+  switch (location.hostname) {
+    case 'localhost':
+      return '/'
+    case 'mejiamanuel57.github.io': return '/britecore-payments-datatable'
+  }
+}
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: generatePath,
       name: 'Home',
       component: Home
     }
